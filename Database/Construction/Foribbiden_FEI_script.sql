f2fc7bb17f44a16f9ea18d463fdf8468e82c6ea6
Gabo-Mac <gabgonlo05@gmail.com>
2025-09-14 03:02:58 -0600

    Design and Database implementation updated

--- Database/Construction/Foribbiden_FEI_script.sql
+++ Database/Construction/Foribbiden_FEI_script.sql
@@ -5,7 +5,13 @@ player_username nvarchar(30) unique,
 player_password nvarchar(64) not null,
 player_email nvarchar(320) unique,
 player_avatar nvarchar(500),
-player_socialmedia nvarchar(500)
+)
+
+CREATE TABLE player_socialmedia (
+social_media int IDENTITY(1,1) primary key,
+player_id int,
+social_link nvarchar(50),
+CONSTRAINT FK_player_socialmedia FOREIGN KEY (player_id) REFERENCES player(player_id)
 )
 
 CREATE TABLE tile (
@@ -15,13 +21,14 @@ type nvarchar(30) not null
 )
 
 CREATE TABLE card_catalog (
-card_type nvarchar(10)
+card_type nvarchar(10) primary key
 )
 
 CREATE TABLE card (
 card_id int identity(1,1) primary key,
 card_name nvarchar(30) not null,
 description nvarchar(150) not null,
+type nvarchar not null,
 type nvarchar(10) not null
 )
 
@@ -39,30 +46,30 @@ CREATE TABLE match_players (
 match_players_id int identity(1,1) primary key,
 match_id int not null,
 player_id int not null,
-CONSTRAINT FK_match FOREIGN KEY (match_id) REFERENCES matches(match_id),
-CONSTRAINT FK_player FOREIGN KEY (player_id) REFERENCES player(player_id)
+CONSTRAINT FK_match_players_match FOREIGN KEY (match_id) REFERENCES matches(match_id),
+CONSTRAINT FK_players_player FOREIGN KEY (player_id) REFERENCES player(player_id)
 )
 
 CREATE TABLE match_tiles (
 match_tiles_id int identity(1,1) primary key,
 match_id int not null,
 tile_id not null,
-CONSTRAINT FK_match FOREIGN KEY (match_id) REFERENCES matches(match_id),
-CONSTRAINT FK_tile FOREIGN KEY (tile_id) REFERENCES tile(tile_id)
+CONSTRAINT FK_match_tiles_match FOREIGN KEY (match_id) REFERENCES matches(match_id),
+CONSTRAINT FK_tile_match_tile FOREIGN KEY (tile_id) REFERENCES tile(tile_id)
 )
 
 CREATE TABLE match_cards (
 match_cards_id int identity(1,1) primary key,
 match_id int not null,
 card_id not null,
-CONSTRAINT FK_match FOREIGN KEY (match_id) REFERENCES matches(match_id),
-CONSTRAINT FK_card FOREIGN KEY (card_id) REFERENCES card(card_id)
+CONSTRAINT FK_match_cards_match FOREIGN KEY (match_id) REFERENCES matches(match_id),
+CONSTRAINT FK_match_cards_card FOREIGN KEY (card_id) REFERENCES card(card_id)
 )
 
 CREATE TABLE match_minigames (
 match_minigames_id int identity(1,1) primary key,
 match_id int not null,
 minigame_id not null,
-CONSTRAINT FK_match FOREIGN KEY (match_id) REFERENCES matches(match_id),
-CONSTRAINT FK_minigame FOREIGN KEY (minigame_id) REFERENCES minigame(minigame_id)
+CONSTRAINT FK_match_minigames_match FOREIGN KEY (match_id) REFERENCES matches(match_id),
+CONSTRAINT FK_match_minigame_minigame FOREIGN KEY (minigame_id) REFERENCES minigame(minigame_id)
 )
