e2efa78dd9332abaec6c1763ed771e53784621f8
leodanortega <leonardo.ortega.teoba@gmail.com>
2025-10-13 07:57:10 -0600

    FIX: Language

--- Construction/Forbbiden.Client/App.xaml.cs
+++ Construction/Forbbiden.Client/App.xaml.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using log4net;
+using log4net.Config;
+using System.IO;
+using System.Threading;
 using System.Windows;
 
 namespace Forbbiden.Client
@@ -11,7 +9,20 @@ namespace Forbbiden.Client
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
+
     public partial class App : Application
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(App));
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var langCode = Forbbiden.Client.Properties.Settings.Default.languageCode;
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(langCode);
+            base.OnStartup(e);
+
+            XmlConfigurator.Configure(new FileInfo("log4net.config"));
+
+            log.Info("App running");
+        }
     }
 }
