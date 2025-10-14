e795ab26fa8a55f4d8edae326fe470af1dcaf4d9
leodanortega <55897583+leodanortega@users.noreply.github.com>
2025-10-13 22:35:11 -0600

    Update: Views

---
+++ Construction/Forbbiden.Client/view/MainPage.xaml.cs
@@ -0,0 +1,148 @@
+﻿using Forbbiden.Client.ProfileManager;
+using log4net;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Forbbiden.Client
+{
+    public partial class MainPage : Page
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(LoginPage));
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            if (Properties.Settings.Default.rememberLogin)
+            {
+                ReloadMainPage();
+            }
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (this.NavigationService != null)
+                {
+                    this.NavigationService.Navigate(new PlayPage());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al abrir la página de juego.");
+                log.Error("MainPage.xaml.cs - PlayButton_Click", ex);
+            }
+        }
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (this.NavigationService != null)
+                {
+                    this.NavigationService.Navigate(new SelectLanguagePage());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al abrir la configuración.");
+                log.Error("MainPage.xaml.cs - SettingsButton_Click", ex);
+            }
+        }
+
+        private void QuitGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var client = new ProfileManagerClient();
+
+                if (!Properties.Settings.Default.rememberLogin)
+                {
+                    if (!client.ClearCurrentLogin())
+                    {
+                        MessageBox.Show("Error al cerrar sesión en la base de datos.");
+                    }
+                }
+
+                Application.Current.Shutdown();
+                log.Info("App closed");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cerrar la aplicación.");
+                log.Error("MainPage.xaml.cs - QuitGameButton_Click", ex);
+            }
+        }
+
+        private void ProfileButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var client = new ProfileManagerClient();
+                Player player = client.GetCurrentLogin();
+
+                if (this.NavigationService != null)
+                {
+                    if (player == null)
+                    {
+                        this.NavigationService.Navigate(new ProfilePage());
+                    }
+                    else
+                    {
+                        this.NavigationService.Navigate(new ProfilePage(player));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al abrir el perfil.");
+                log.Error("MainPage.xaml.cs - ProfileButton_Click", ex);
+            }
+        }
+
+        private void LogInButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (this.NavigationService != null)
+                {
+                    this.NavigationService.Navigate(new LoginPage());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al abrir la página de login.");
+                log.Error("MainPage.xaml.cs - LogInButton_Click", ex);
+            }
+        }
+
+        public void ReloadMainPage()
+        {
+            try
+            {
+                var client = new ProfileManagerClient();
+                Player player = client.GetCurrentLogin();
+
+                if (player != null)
+                {
+                    txtBkUser.Text = player.PlayerUsername;
+
+                    ImageBrush avatar = new ImageBrush(
+                        new System.Windows.Media.Imaging.BitmapImage(
+                            new Uri(player.PlayerAvatarPath, UriKind.RelativeOrAbsolute)
+                        )
+                    );
+
+                    imgAvatar.Fill = avatar;
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error("MainPage.xaml.cs - ReloadMainPage", ex);
+            }
+        }
+    }
+}
