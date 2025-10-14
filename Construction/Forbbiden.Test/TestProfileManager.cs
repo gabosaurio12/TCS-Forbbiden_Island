f2d83e2c1a1f201992e65b9873f536ee5e5ad8b9
GaboSaurio <gabgonlo05@gmail.com>
2025-10-13 22:01:25 -0600

    FEAT: First tests and test cases

--- Construction/Forbbiden.Test/TestProfileManager.cs
+++ Construction/Forbbiden.Test/TestProfileManager.cs
@@ -1,20 +1,65 @@
 using ProfileManager;
+using NUnit.Framework;
 
 namespace Forbbiden.Test
 {
+    [TestFixture]
     public class TestProfileManager
     {
 
         [SetUp]
         public void Setup()
         {
-            var profileManager = new ProfileManagerClient();
+            var client = new ProfileManagerClient();
+
+            Player player = new Player
+            {
+                PlayerUsername = "testUser",
+                PlayerPassword = "T3st_pass",
+                PlayerEmail = "testuser@email.net"
+            };
+
+            client.SignUpAsync(player);
         }
 
         [Test]
-        public void TestIsEmailAvailableSuccess()
+        public async Task TestIsEmailAvailableSuccess()
         {
-            Assert.Pass();
+            var client = new ProfileManagerClient();
+            string email = "randomEmail@email.net";
+
+            var result = await client.IsEmailAvailableAsync(email);
+            Assert.That(result, Is.True, "result should be true");
+        }
+
+        [Test]
+        public async Task TestIsEmailAvailableInvalidEmail()
+        {
+            var client = new ProfileManagerClient();
+            string email = "randomEmail.com";
+
+            var result = await client.IsEmailAvailableAsync(email);
+            Assert.That(result, Is.False, "result should be false");
+        }
+
+        [Test]
+        public async Task TestIsEmailAvailableEmptyEmail()
+        {
+            var client = new ProfileManagerClient();
+            string email = "";
+
+            var result = await client.IsEmailAvailableAsync(email);
+            Assert.That(result, Is.False, "result should be false");
+        }
+
+        [Test]
+        public async Task TestIsEmailAvailableTakenEmail()
+        {
+            var client = new ProfileManagerClient();
+            string email = "testuser@email.net";
+
+            var result = await client.IsEmailAvailableAsync(email);
+            Assert.That(result, Is.False, "result should be false");
         }
     }
 }
