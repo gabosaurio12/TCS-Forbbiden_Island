66ba4a27270b12f3e18d94e36559c5e6af869aab
Gabo-Mac <gabgonlo05@gmail.com>
2025-10-22 23:11:01 -0600

    New glass image

---
+++ Construction/Forbbiden.Client/logic/LoginOriginator.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Forbbiden.Client
+{
+    public class LoginOriginator
+    {
+        private string _username { get; set; }
+        private string _email { get; set; }
+
+        public LoginOriginator(string username, string email)
+        {
+            _username = username;
+        }
+
+        public LoginOriginator(string username, string email)
+        {
+            _username = username;
+            _email = email;
+        }
+
+        public SaveSnapshot()
+        {
+        }
+
+        public RestoreSnapshot()
+        {
+        }
+    }
+}
