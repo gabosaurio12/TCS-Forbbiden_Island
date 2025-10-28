bdf25f1d24becc99bf86b86b0b471a048a5c38bf
Gabo-Mac <gabgonlo05@gmail.com>
2025-10-27 19:32:09 -0600

    pulling

--- Construction/Forbbiden.Client/view/MainWindow.xaml.cs
+++ Construction/Forbbiden.Client/view/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ namespace Forbbiden.Client.view
     /// <summary>
     /// MainWindow.xaml interaction logic
     /// </summary>
+    
     public partial class MainWindow : NavigationWindow
     {
         public MainWindow()
