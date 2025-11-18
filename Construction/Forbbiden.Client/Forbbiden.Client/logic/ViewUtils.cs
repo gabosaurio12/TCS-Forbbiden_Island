b7660a58edaa5af63cd5d7920d3c552033bf87e7
GaboSaurio <gabgonlo05@gmail.com>
2025-11-18 14:28:51 -0600

    FEAT: xaml logic

---
+++ Construction/Forbbiden.Client/Forbbiden.Client/logic/ViewUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Forbbiden.Client.logic
+{
+    public class ViewUtils
+    {
+
+        public static BitmapImage GetImage(string imagePath)
+        {
+            var bmp = new BitmapImage();
+            bmp.BeginInit();
+            bmp.CacheOption = BitmapCacheOption.OnLoad;
+            bmp.UriSource = new Uri(imagePath, UriKind.Absolute);
+            bmp.EndInit();
+
+            return bmp;
+        }
+    }
+}
